ea5569a0e0f11475c09e2406960d32882cf14003
Chandan <chandankrckc@gmail.com>
2024-04-11 04:28:56 +0530

    Understanding Props

---
+++ dist/index.b2d3faae.js
@@ -0,0 +1,13 @@
+//const heading = React.createElement("h1" , {} , "Hello From react World");
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "This is World"),
+    React.createElement("h2", {}, "This is India")
+]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.b2d3faae.js.map
